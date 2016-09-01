054d64217228e1150400d5a1f58a3dcbb1243d8b
gabrielstuff <gabriel@soixantecircuits.fr>
2016-09-01 10:49:03 +0200

    adding dist file

---
+++ dist/config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Default config
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var config = {
+  zeroconfName: 'spacebro',
+  channelName: null,
+  clientName: null,
+  packers: [],
+  unpackers: [],
+  sendBack: true,
+  verbose: true
+};
+
+exports.default = config;
---
+++ dist/log.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _config = require('./config');
+
+var _config2 = _interopRequireDefault(_config);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// = Helpers ===
+function log() {
+  if (!_config2.default.verbose) {
+    return;
+  } else {
+    var _console;
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    (_console = console).log.apply(_console, ['SpaceBro -'].concat(args));
+  }
+}
+
+exports.default = log;
