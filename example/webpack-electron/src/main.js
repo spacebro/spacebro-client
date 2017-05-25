bdee2c382715d9af26c1a70c4adfd6faad98a057
gabrielstuff <gabriel@soixantecircuits.fr>
2017-05-25 17:40:56 +0200

    🍌 add webpack electron example

---
+++ example/webpack-electron/src/main.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const spacebroClient = require('spacebro-client')
+
+function addTextNode(text) {
+  let logDiv = document.getElementById('log')
+  logDiv.insertAdjacentHTML('beforeend', `<p>${text}</p>`)
+}
+
+spacebroClient.connect('127.0.0.1', 8888, {
+  clientName: 'foo',
+  channelName: 'bar'
+})
+
+spacebroClient.on('connect', () => {
+  addTextNode('connected')
+})
+
+const events = ['hello', 'world']
+events.forEach((event) => {
+  spacebroClient.on(event, (data) => {
+    addTextNode(JSON.stringify(data))
+  })
+})
+
+setInterval(() => { spacebroClient.emit('hello', { hello: 'world' }) }, 3000)
+
---
+++ example/webpack-electron/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const webpack = require('webpack')
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] },
+        }]
+      }
+    ]
+  }
+}
