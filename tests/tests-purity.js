e7e8ffdfa3fed04eb77e2a377090c8a7eff1d0b6
Olivier FAURE <olivier.faure@epitech.eu>
2017-06-16 18:07:48 +0200

    :white_check_mark: Add purity unit test

---
+++ tests/tests-purity.js
@@ -0,0 +1,35 @@
+import test from 'ava'
+import sleep from 'sleep-promise'
+
+import spacebroClient from '../src/spacebro-client.js'
+
+test.failing.serial('clientName', async t => {
+  console.log("Hello")
+  t.pass()
+
+  spacebroClient.connect('spacebro.space', 3333, {
+    clientName: 'client1',
+    channelName: 'channelTest',
+    verbose: false
+  })
+
+  spacebroClient.connect('spacebro.space', 3333, {
+    clientName: 'client2',
+    channelName: 'channelTest',
+    verbose: false,
+    sendBack: false
+  })
+
+  await sleep(200) // waits 200ms for connection to be estalished
+
+  const clients = []
+
+  spacebroClient.on('testMessage', (data) => {
+    clients.push(data._from)
+  })
+  spacebroClient.emit('testMessage')
+
+  await sleep(200) // waits 200ms for messages to be sent
+
+  t.deepEqual(clients, ['client1', 'client2'])
+})
