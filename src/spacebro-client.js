c438bda55a1b4488653c013ae676104c58d9e3c8
Olivier FAURE <olivier.faure@epitech.eu>
2017-06-27 18:40:21 +0200

    :gem: Refactor code into OOP model

--- src/spacebro-client.js
+++ src/spacebro-client.js
@@ -3,11 +3,11 @@
 import wildcard from 'socketio-wildcard'
 import io from 'socket.io-client'
 import Signal from 'signals'
-import logger from './logger'
+import Logger from './logger'
 
 const patch = wildcard(io.Manager)
 
-let config = {
+const defaultConfig = {
   channelName: null,
   clientName: null,
   packers: [],
@@ -17,181 +17,6 @@ let config = {
   multiService: false
 }
 
-let connected = false
-let unpackers = []
-let packers = []
-let sockets = []
-let events = {}
-
-function connect (_address, _port, _options) {
-  let address = (typeof _address === 'object') ? null : _address
-  let port = (typeof _address === 'object') ? null : (typeof _port === 'object') ? null : _port
-  let options = (typeof _address === 'object') ? _address : (typeof _port === 'object') ? _port : _options
-
-  Object.assign(config, options)
-  logger.setup(config.verbose)
-  logger.log(`Trying to connect on ${address}:${port} with config:\n`, config)
-
-  if (address && port) {
-    initSocketIO(address, port, null)
-  } else {
-    logger.warn('please provide a server address and port')
-  }
-
-  for (let packer of config.packers) {
-    addPacker(packer.handler, packer.priority, packer.eventName)
-  }
-  for (let unpacker of config.unpackers) {
-    addUnpacker(unpacker.handler, unpacker.priority, unpacker.eventName)
-  }
-}
-
-function initSocketIO (address, port) {
-  let parsedURI = require('url').parse(address)
-  let protocol = parsedURI.protocol ? '' : 'ws://'
-  let url = `${protocol}${address}:${port}`
-
-  let socket = io(url)
-
-  patch(socket)
-
-  socket
-    .on('connect', () => {
-      connected = true
-      logger.log('socket connected')
-      if (sockets.length < 1) {
-        sockets.push(socket)
-      } else {
-        sockets.forEach((el) => {
-          if (el.id !== socket.id) {
-            sockets.push(socket)
-          }
-        })
-      }
-      socket.emit('register', {
-        clientName: config.clientName,
-        channelName: config.channelName,
-        events: config.events
-      })
-      events['connect'] && events['connect'].dispatch(socket)
-    })
-    .on('connect_error', (err) => {
-      logger.warn('error', err)
-      connected = false
-      events['connect_error'] && events['connect_error'].dispatch(err)
-    })
-    .on('connect_timeout', () => {
-      logger.warn('connection timeout')
-      events['connect_timeout'] && events['connect_timeout'].dispatch()
-    })
-    .on('error', (err) => {
-      logger.warn('error', err)
-      connected = false
-      events['error'] && events['error'].dispatch(err)
-    })
-    .on('disconnect', (err) => {
-      logger.log('socket down')
-      events['disconnect'] && events['disconnect'].dispatch(err)
-      connected = false
-    })
-    .on('reconnect', (data) => {
-      logger.log('socket reconnected')
-      events['reconnect'] && events['reconnect'].dispatch(data)
-      connected = true
-    })
-    .on('reconnect_attempt', (attempt) => {
-      logger.log(`socket reconnect attempt: ${attempt}`)
-      events['reconnect_attempt'] && events['reconnect_attempt'].dispatch(attempt)
-    })
-    .on('reconnecting', (attempt) => {
-      logger.log(`socket try to reconnect, attempt: ${attempt}`)
-      events['reconnecting'] && events['reconnecting'].dispatch(attempt)
-    })
-    .on('reconnect_error', (err) => {
-      logger.warn(`socket reconnection error`)
-      logger.warn(err)
-      events['reconnect_error'] && events['reconnect_error'].dispatch(err)
-    })
-    .on('reconnect_failed', (err) => {
-      logger.warn(`socket can't reconnect`)
-      logger.warn(err)
-      events['reconnect_failed'] && events['reconnect_failed'].dispatch(err)
-    })
-
-    .on('*', ({ data }) => {
-      let [eventName, args] = data
-      if (!config.sendBack && args._from === config.clientName) {
-      } else if (events[eventName]) {
-        logger.log(`socket received ${eventName} with data:`, args)
-        for (let unpack of filterHooks(eventName, unpackers)) {
-          const unpacked = unpack({ eventName, data: args })
-          args = unpacked || args
-        }
-        events[eventName].dispatch(args)
-      }
-    })
-}
-
-function disconnect () {
-  connected = false
-  unpackers = []
-  packers = []
-  for (const socket of sockets) {
-    socket.close()
-  }
-  sockets = []
-  for (const eventName of events.keys()) {
-    events[eventName].dispose()
-  }
-  events = {}
-}
-
-function addPacker (handler, priority, eventName) {
-  addHook(packers, eventName, handler, priority)
-}
-function addUnpacker (handler, priority, eventName) {
-  addHook(unpackers, eventName, handler, priority)
-}
-
-function emit (eventName, data = {}) {
-  // null is a type of Object. so we have to check null and undefined with loosy compare
-  if (typeof data !== 'object' || data == null) {
-    data = {data: data}
-    data.altered = true
-  }
-  sendTo(eventName, null, data)
-}
-
-function sendTo (eventName, to = null, data = {}) {
-  if (connected) {
-    data._to = to
-    data._from = config.clientName
-    for (let pack of filterHooks(eventName, packers)) {
-      data = pack({eventName, data}) || data
-    }
-    for (let socket of sockets) {
-      socket.emit(eventName, data)
-    }
-  } else {
-    logger.warn('can\'t emit, not connected.')
-  }
-}
-
-// Reception
-function on (eventName, handler, handlerContext, priority) {
-  events[eventName] = new Signal()
-  events[eventName].add(handler, handlerContext, priority)
-}
-
-function once (eventName, handler, handlerContext, priority) {
-  events[eventName] = new Signal()
-  events[eventName].addOnce(handler, handlerContext, priority)
-}
-
-function off (eventName) {
-  delete events[eventName]
-}
-
 function filterHooks (eventName, hooks) {
   return hooks
     .filter(hook => [eventName, '*'].indexOf(hook.eventName) !== -1)
@@ -199,11 +24,234 @@ function filterHooks (eventName, hooks) {
     .map(hook => hook.handler)
 }
 
-function addHook (hooks, eventName = '*', handler, priority = 0) {
-  hooks.push({eventName, handler, priority})
+class SpacebroClient {
+  constructor (address, port, options) {
+    this.config = Object.assign({}, defaultConfig, options)
+    this.logger = new Logger(this.config.verbose)
+
+    for (const packer of this.config.packers) {
+      this.addPacker(packer.handler, packer.priority, packer.eventName)
+    }
+    for (const unpacker of this.config.unpackers) {
+      this.addUnpacker(unpacker.handler, unpacker.priority, unpacker.eventName)
+    }
+
+    if (typeof address !== 'string') {
+      throw new Error('address must be a valid string')
+    }
+    if (!(port > 0)) {
+      throw new Error('port must be a positive integer')
+    }
+
+    this.events = {}
+    this.connected = false
+    this.socket = null
+
+    this.logger.log(
+      `Trying to connect on ${address}:${port} with config:\n`, this.config
+    )
+    this.initSocketIO(address, port)
+  }
+
+  initSocketIO (address, port) {
+    let parsedURI = require('url').parse(address)
+    let protocol = parsedURI.protocol ? '' : 'ws://'
+    let url = `${protocol}${address}:${port}`
+
+    let socket = io(url)
+
+    patch(socket)
+
+    socket
+      .on('connect', () => {
+        this.connected = true
+        this.logger.log('socket connected')
+        lastSocket = socket
+        socket.emit('register', {
+          clientName: this.config.clientName,
+          channelName: this.config.channelName
+        })
+        this.events['connect'] && this.events['connect'].dispatch(socket)
+      })
+      .on('connect_error', (err) => {
+        this.logger.warn('error', err)
+        this.connected = false
+        this.events['connect_error'] && this.events['connect_error'].dispatch(err)
+      })
+      .on('connect_timeout', () => {
+        this.logger.warn('connection timeout')
+        this.events['connect_timeout'] && this.events['connect_timeout'].dispatch()
+      })
+      .on('error', (err) => {
+        this.logger.warn('error', err)
+        this.connected = false
+        this.events['error'] && this.events['error'].dispatch(err)
+      })
+      .on('disconnect', (err) => {
+        this.logger.log('socket down')
+        this.events['disconnect'] && this.events['disconnect'].dispatch(err)
+        this.connected = false
+      })
+      .on('reconnect', (data) => {
+        this.logger.log('socket reconnected')
+        this.events['reconnect'] && this.events['reconnect'].dispatch(data)
+        this.connected = true
+      })
+      .on('reconnect_attempt', (attempt) => {
+        this.logger.log(`socket reconnect attempt: ${attempt}`)
+        this.events['reconnect_attempt'] && this.events['reconnect_attempt'].dispatch(attempt)
+      })
+      .on('reconnecting', (attempt) => {
+        this.logger.log(`socket try to reconnect, attempt: ${attempt}`)
+        this.events['reconnecting'] && this.events['reconnecting'].dispatch(attempt)
+      })
+      .on('reconnect_error', (err) => {
+        this.logger.warn('socket reconnection error')
+        this.logger.warn(err)
+        this.events['reconnect_error'] && this.events['reconnect_error'].dispatch(err)
+      })
+      .on('reconnect_failed', (err) => {
+        this.logger.warn('socket cannot reconnect')
+        this.logger.warn(err)
+        this.events['reconnect_failed'] && this.events['reconnect_failed'].dispatch(err)
+      })
+
+      .on('*', ({ data }) => {
+        let [eventName, args] = data
+        if (!this.config.sendBack && args._from === this.config.clientName) {
+          return
+        }
+        if (this.events[eventName]) {
+          this.logger.log(`socket received ${eventName} with data:`, args)
+          for (let unpack of filterHooks(eventName, this.unpackers)) {
+            const unpacked = unpack({ eventName, data: args })
+            args = unpacked || args
+          }
+          this.events[eventName].dispatch(args)
+        }
+      })
+  }
+
+  disconnect () {
+    this.connected = false
+    this.unpackers = []
+    this.packers = []
+    for (const eventName of this.events.keys()) {
+      this.off(eventName)
+    }
+  }
+
+  addPacker (handler, priority, eventName) {
+    this.packers.push({ eventName, handler, priority })
+  }
+  addUnpacker (handler, priority, eventName) {
+    this.unpackers.push({ eventName, handler, priority })
+  }
+
+  emit (eventName, data = {}) {
+    // null is a type of Object. so we have to check null and undefined with loosy compare
+    if (typeof data !== 'object' || data == null) {
+      data = {data: data}
+      data.altered = true
+    }
+    this.sendTo(eventName, null, data)
+  }
+
+  sendTo (eventName, to = null, data = {}) {
+    if (this.connected) {
+      data._to = to
+      data._from = this.config.clientName
+      for (let pack of filterHooks(eventName, this.packers)) {
+        data = pack({eventName, data}) || data
+      }
+      this.socket.emit(eventName, data)
+    } else {
+      this.logger.warn('can\'t emit, not connected.')
+    }
+  }
+
+  // Reception
+  on (eventName, handler, handlerContext, priority) {
+    if (this.events[eventName]) {
+      this.logger.warn(`Signal ${eventName} already exists`)
+    }
+    this.events[eventName] = new Signal()
+    this.events[eventName].add(handler, handlerContext, priority)
+  }
+
+  once (eventName, handler, handlerContext, priority) {
+    if (this.events[eventName]) {
+      this.logger.warn(`Signal ${eventName} already exists`)
+    }
+    this.events[eventName] = new Signal()
+    this.events[eventName].addOnce(handler, handlerContext, priority)
+  }
+
+  off (eventName) {
+    this.events[eventName].dispose()
+    delete this.events[eventName]
+  }
+}
+
+let lastSocket = null
+
+function connect (address, port, options) {
+  if (lastSocket) {
+    console.warn('A SpacebroClient socket is already open')
+  }
+  lastSocket = new SpacebroClient(address, port, options)
+}
+
+function checkSocket () {
+  if (!lastSocket) {
+    throw new Error('No SpacebroClient socket is open')
+  }
+}
+
+function disconnect () {
+  checkSocket()
+  lastSocket.disconnect()
+  lastSocket = null
+}
+
+function addPacker (handler, priority, eventName) {
+  checkSocket()
+  lastSocket.addPacker(handler, priority, eventName)
+}
+
+function addUnpacker (handler, priority, eventName) {
+  checkSocket()
+  lastSocket.addUnpacker(handler, priority, eventName)
+}
+
+function emit (eventName, data = {}) {
+  checkSocket()
+  lastSocket.emit(eventName, data)
+}
+
+function sendTo (eventName, to = null, data = {}) {
+  checkSocket()
+  lastSocket.sendTo(eventName, to, data)
+}
+
+// Reception
+function on (eventName, handler, handlerContext, priority) {
+  checkSocket()
+  lastSocket.on(eventName, handler, handlerContext, priority)
+}
+
+function once (eventName, handler, handlerContext, priority) {
+  checkSocket()
+  lastSocket.once(eventName, handler, handlerContext, priority)
+}
+
+function off (eventName) {
+  checkSocket()
+  lastSocket.off(eventName)
 }
 
 export default {
+  SpacebroClient,
   connect,
   disconnect,
   addPacker,
