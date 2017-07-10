7108af143d38867a5aa7f45a49e535c3339727e1
Olivier FAURE <olivier.faure@epitech.eu>
2017-07-10 11:15:54 +0200

    :white_check_mark: Rewrite sendback tests

--- test/tests-sendback.js
+++ test/tests-sendback.js
@@ -19,8 +19,8 @@ function inspect (obj) {
   return util.inspect(obj, {showHidden: false, depth: null})
 }
 
-test('connect - Sendback', async (t) => {
-  const client = connectSendback('sendBack')
+test('connect - Sendback with object', async (t) => {
+  const client = connectSendback('sendBack-object')
 
   t.plan(0)
 
@@ -31,15 +31,50 @@ test('connect - Sendback', async (t) => {
   await sleep(5000)
 })
 
-test.failing('connect - Sendback with special data', async (t) => {
-  const client = connectSendback('sendBack-special-data')
+test('connect - Sendback with no data', async (t) => {
+  const client = connectSendback('sendBack-no-data')
 
   t.plan(0)
 
   client.on('sendBack', (data) => t.fail(`Received own data: ${inspect(data)}`))
   client.on('connect', () => {
     client.emit('sendBack')
+  })
+  await sleep(5000)
+})
+
+test.failing('connect - Sendback with string', async (t) => {
+  const client = connectSendback('sendBack-string')
+
+  t.plan(0)
+
+  client.on('sendBack', (data) => t.fail(`Received own data: ${inspect(data)}`))
+  client.on('connect', () => {
     client.emit('sendBack', 'helloWorld')
+  })
+  await sleep(5000)
+})
+
+test.failing('connect - Sendback with Number', async (t) => {
+  const client = connectSendback('sendBack-number')
+
+  t.plan(0)
+
+  client.on('sendBack', (data) => t.fail(`Received own data: ${inspect(data)}`))
+  client.on('connect', () => {
+    client.emit('sendBack', 42)
+  })
+  await sleep(5000)
+})
+
+test.failing('connect - Sendback with Array', async (t) => {
+  const client = connectSendback('sendBack-array')
+
+  t.plan(0)
+
+  client.on('sendBack', (data) => t.fail(`Received own data: ${inspect(data)}`))
+  client.on('connect', () => {
+    client.emit('sendBack', [1, 2, 3])
   })
   await sleep(5000)
 })
